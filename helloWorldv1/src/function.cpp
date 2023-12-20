c1f323b1a30198a59707f8748622efed9a84676b
makaka <75128685+makk521@users.noreply.github.com>
2023-12-20 21:56:07 +0800

    add helloword1

---
+++ helloWorldv1/src/function.cpp
@@ -0,0 +1,5 @@
+#include "function.hpp"
+
+void fun1(){
+    std::cout << "Hello World!" << std::endl;
+}
---
+++ helloWorldv1/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "function.hpp"
+
+int main(){
+    fun1();
+    return 0;
+}
